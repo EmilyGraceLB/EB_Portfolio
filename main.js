6881c283aeddbb3826e6791018805bdc692c34b3
Emily <145925732+EmilyGraceLB@users.noreply.github.com>
2025-04-24 23:47:28 +0100

    added buttons and images to replace text links

---
+++ main.js
@@ -0,0 +1,19 @@
+document.getElementById("MHDPHDButton").onclick = function () {
+    window.location.href = "MSDPHD.html";
+};
+
+document.getElementById("MPIEBtn").onclick = function () {
+    window.location.href = "MPIE.html";
+};
+
+document.getElementById("UXDbtn").onclick = function () {
+    window.location.href = "UXD.html";
+};
+
+document.getElementById("DIMbtn").onclick = function () {
+    window.location.href = "DIM.html";
+};
+
+document.getElementById("PDMbtn").onclick = function () {
+    window.location.href = "PDM.html";
+};
